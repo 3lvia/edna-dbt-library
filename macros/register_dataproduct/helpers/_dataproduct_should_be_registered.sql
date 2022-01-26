263e9375a298c6ade1876ae31e5adcff06b0ffbe
SigridAndersen <35346389+SigridAndersen@users.noreply.github.com>
2022-01-26 11:29:00 +0100

    Update _dataproduct_should_be_registered.sql

--- macros/register_dataproduct/helpers/_dataproduct_should_be_registered.sql
+++ macros/register_dataproduct/helpers/_dataproduct_should_be_registered.sql
@@ -2,7 +2,7 @@
   {%- if dataprodconfig is defined and dataprodconfig is not none -%}
     {{- return(true) -}}
   {%- else -%}
-    {%- if edna_dbt_lib._is_registered_dataproduct() -%}
+    {%- if edna_dbt_lib._is_registered_dataproduct(product_id) -%}
       {{ exceptions.raise_compiler_error("Can't unregister dataproduct.") }}
     {%- endif -%}
     {{- return(false) -}}
