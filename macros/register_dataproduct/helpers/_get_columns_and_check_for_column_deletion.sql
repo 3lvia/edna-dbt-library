af725150234fcda57d4baf4704b81e78756df63b
SigridAndersen <35346389+SigridAndersen@users.noreply.github.com>
2022-01-26 11:38:39 +0100

    Update _get_columns_and_check_for_column_deletion.sql

--- macros/register_dataproduct/helpers/_get_columns_and_check_for_column_deletion.sql
+++ macros/register_dataproduct/helpers/_get_columns_and_check_for_column_deletion.sql
@@ -1,5 +1,5 @@
 {%- macro _get_columns_and_check_for_column_deletion(is_registered, bq_project, identifier, compiled_sql) -%}
-  {%- set tmp_relation = edna_dbt_lib.create_tmp_relation(bq_project, identifier, compiled_sql) -%}
+  {%- set tmp_relation = edna_dbt_lib._create_tmp_relation(bq_project, identifier, compiled_sql) -%}
   {%- set new_columns = adapter.get_columns_in_relation(tmp_relation) -%}
   {%- if is_registered_dataproduct -%}
     {%- set missing_columns = adapter.get_missing_columns(this, tmp_relation) -%}
