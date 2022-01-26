d07b801f8776f524214acfceac2675e6c4666848
SigridAndersen <35346389+SigridAndersen@users.noreply.github.com>
2022-01-26 11:51:26 +0100

    Update _get_columns_and_check_for_column_deletion.sql

--- macros/register_dataproduct/helpers/_get_columns_and_check_for_column_deletion.sql
+++ macros/register_dataproduct/helpers/_get_columns_and_check_for_column_deletion.sql
@@ -3,11 +3,12 @@
   {%- set new_columns = adapter.get_columns_in_relation(tmp_relation) -%}
   {%- if is_registered_dataproduct -%}
     {%- set missing_columns = adapter.get_missing_columns(this, tmp_relation) -%}
-    {%- do adapter.drop_relation(tmp_relation) -%}
     {%- if missing_columns | length > 0 -%}
+      {%- do adapter.drop_relation(tmp_relation) -%}
       {#- Should we allow flag to force change of dataproduct? -#}
       {{ exceptions.raise_compiler_error("Schema of registered dataproduct can't be changed. Missing columns: " ~ missing_columns | map(attribute="name") |join(', ')) }}
     {%- endif -%}
   {%- endif -%}
+  {%- do adapter.drop_relation(tmp_relation) -%}
   {{- return(new_columns) -}}
 {%- endmacro -%}
