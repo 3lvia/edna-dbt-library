9158dcc643795d922d2a4c4e3d0bdb8b9d1a3e54
SigridAndersen <35346389+SigridAndersen@users.noreply.github.com>
2022-02-01 07:54:08 +0100

    Update generate_schema_name.sql

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,8 +1,8 @@
-{% macro generate_schema_name(custom_schema_name, node) -%}
-  {%- set dataproduct_group = node.path.split('/')[0] -%}
-  {%- if node.path.split('/')[1] == 'dataproduct' -%}
-    {{ project_name }}_{{ dataproduct_group }}
-  {%- else -%}
-    {{ project_name }}_{{ dataproduct_group }}_curated
-  {% endif %}
+{% macro generate_schema_name(custom_schema_name, node) %}
+    {% set dataproduct_group = node.path.split('/')[0] %}
+    {% if node.path.split('/')[1] == 'dataproduct' %}
+        {{ project_name }}_{{ dataproduct_group }}
+    {%- else -%}
+        {{ project_name }}_{{ dataproduct_group }}_curated
+    {% endif %}
 {%- endmacro %}
