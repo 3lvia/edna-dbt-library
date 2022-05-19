9c564da087e61a80f347200b1ba35a8b57c48206
SigridAndersen <35346389+SigridAndersen@users.noreply.github.com>
2022-05-19 17:24:24 +0200

    Update validate_dataproduct.sql

--- macros/product_registration/validate_dataproduct.sql
+++ macros/product_registration/validate_dataproduct.sql
@@ -76,11 +76,10 @@
     {%- endif -%}
 
     {% set model_definition_columns = config.model.columns if edna_dbt_lib.is_defined(config.model.columns) else {} %}
-    {% for row in new_columns %}
-        {% set column_name = row['field_path'] %}
-        {% set model_column = model_definition_columns.get(column_name) %}
+    {% for column in new_columns %}
+        {% set model_column = model_definition_columns.get(column.name) %}
         {% if not edna_dbt_lib.is_defined(model_column.description) %}
-            {{ exceptions.raise_compiler_error("Dataproduct columns must have a description, missing description for {}".format(column_name)) }}
+            {{ exceptions.raise_compiler_error("Dataproduct columns must have a description, missing description for {}".format(column.name)) }}
         {% endif %}
     {% endfor %}
 {%- endmacro -%}
