c04186c800c90ecc79289bae39d415817f77d009
SigridAndersen <sigrid.0489@gmail.com>
2022-08-18 14:03:26 +0200

    Update register_dataproduct_metadata.sql

--- macros/product_registration/register_dataproduct_metadata.sql
+++ macros/product_registration/register_dataproduct_metadata.sql
@@ -24,6 +24,7 @@
             {% set preview_where_clause = dataprodconfig.get('previewWhereClause') %}
             {{ log("!!", true)}}
             {{ log(preview_where_clause == None, true)}}
+            {{ log(preview_where_clause is none, true)}}
 
             {% do edna_dbt_lib._upsert_dataproduct_entry(description, displayName, domain, dataproduct_group,
                                             bq_dataset, bq_tablename, dbt_id, owner, columns, labels, size_info, preview_where_clause) %}
