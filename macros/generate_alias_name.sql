6b5417231e4d9e34a94997cb8ef7188c48e769c5
SigridAndersen <sigrid.0489@gmail.com>
2022-10-12 10:59:26 +0200

    Update generate_alias_name.sql

--- macros/generate_alias_name.sql
+++ macros/generate_alias_name.sql
@@ -2,7 +2,7 @@
     {%- set dataprodconfig = node.config.get('dataproduct') -%}
 
     {%- if edna_dbt_lib.is_defined(dataprodconfig) and edna_dbt_lib.is_defined(dataprodconfig.get('version')) -%}
-        {%- set v = "_v" ~ (dataprodconfig.get('version') | replace(".", "-")) -%}
+        {%- set v = "_v" ~ (dataprodconfig.get('version') | trim('.0') | replace(".", "-")) -%}
     {%- endif -%}
 
     {%- if custom_alias_name is none -%}
