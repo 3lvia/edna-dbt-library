8456d11fbeb040df67d8144893c4bf624eb63c03
SigridAndersen <sigrid.0489@gmail.com>
2022-10-14 10:44:13 +0200

    Update generate_alias_name.sql

--- macros/generate_alias_name.sql
+++ macros/generate_alias_name.sql
@@ -3,6 +3,9 @@
 
     {%- if edna_dbt_lib.is_defined(dataprodconfig) and edna_dbt_lib.is_defined(dataprodconfig.get('version')) -%}
         {%- set v = "_v" ~ (dataprodconfig.get('version') | trim('.0') | replace(".", "-")) -%}
+        {% if v = "_v1" %}
+            {% set v = "" %}
+        {% endif %}
     {%- endif -%}
 
     {%- if custom_alias_name is none -%}
