79020b47cd5bef29afd85e8af10ecae41583d7b2
Haakon Haave <haakon.haave@elvia.no>
2025-08-04 14:29:55 +0200

    feat: update generate schema name macro for dbt fusion

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,21 +1,23 @@
 {% macro generate_schema_name(custom_schema_name, node) %}
-    
+
     {%- set default_schema = target.schema -%}
-    
+
     {% if env_var('DBT_CLOUD_INVOCATION_CONTEXT', '') == 'ci'
         and env_var('DBT_PROJECT_IS_POC', 'false') == 'true' %}
-        
+
         {{ default_schema }}
 
     {%- elif env_var('DBT_CLOUD_INVOCATION_CONTEXT', '') == 'dev'
         and env_var('DBT_PROJECT_IS_POC', 'false') == 'true' -%}
-        
+
         dbt_user_{{ env_var('DBT_USER_ID', '') }}
 
     {%- else -%}
 
-        {% set dataproduct_group = node.path.split('/')[0] %}
-        {% if node.path.split('/')[1] == 'dataproduct' %}
+        {% set dataproduct_group = node.fqn[1] %}
+        {% set layer = node.fqn[2] %}
+
+        {% if layer == 'dataproduct' %}
             {{ project_name }}_{{ dataproduct_group }}
         {%- else -%}
             {{ project_name }}_{{ dataproduct_group }}_curated
